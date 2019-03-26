32df9c5b9a0c4c3da977528df2da681cbcb76bae
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-25 22:49:03 -0500

    Building NoteDetailVC

---
+++ touchyFeelyGoodness/Controller/NoteDetailVC.swift
@@ -0,0 +1,21 @@
+//
+//  NoteDetailVC.swift
+//  touchyFeelyGoodness
+//
+//  Created by Ricardo Herrera Petit on 3/25/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func lockNoteBtnWasPressed(_ sender: Any) {
+    }
+}
